a500b65871e21a913e409d4e64b8045468dee981
bunnie <bunnie@kosagi.com>
2022-03-03 22:57:39 +0800

    start to shim in some libc functions that are missing

--- services/espeak/sys/build.rs
+++ services/espeak/sys/build.rs
@@ -8,38 +8,40 @@ fn main() {
     set_var("CC", "riscv64-unknown-elf-gcc");  // set the compiler to what's installed on the system
 
     let espeak_srcs = vec![
+        "espeak-ng/src/libespeak-ng/translate.c",
+        "espeak-ng/src/libespeak-ng/speech.c",
+        "espeak-ng/src/libespeak-ng/wavegen.c",
+        "espeak-ng/src/libespeak-ng/synthesize.c",
+        "espeak-ng/src/libespeak-ng/dictionary.c",
+        "espeak-ng/src/libespeak-ng/voices.c",
+        "espeak-ng/src/libespeak-ng/synthdata.c",
+        "espeak-ng/src/libespeak-ng/readclause.c",
+        "espeak-ng/src/libespeak-ng/numbers.c",
+        "espeak-ng/src/libespeak-ng/setlengths.c",
+        "espeak-ng/src/libespeak-ng/tr_languages.c",
+        "espeak-ng/src/libespeak-ng/encoding.c",
+        "espeak-ng/src/libespeak-ng/intonation.c",
+        "espeak-ng/src/libespeak-ng/synth_mbrola.c",
+        "espeak-ng/src/libespeak-ng/phoneme.c",
+        "espeak-ng/src/libespeak-ng/phonemelist.c",
+        "espeak-ng/src/libespeak-ng/compiledict.c",
+        "espeak-ng/src/libespeak-ng/mnemonics.c",
+        "espeak-ng/src/libespeak-ng/error.c",
+        "espeak-ng/src/libespeak-ng/libc.c",
         "espeak-ng/src/ucd-tools/src/case.c",
         "espeak-ng/src/ucd-tools/src/categories.c",
         "espeak-ng/src/ucd-tools/src/ctype.c",
         "espeak-ng/src/ucd-tools/src/proplist.c",
-        "espeak-ng/src/ucd-tools/src/scripts.c",
-        "espeak-ng/src/ucd-tools/src/tostring.c",
-        "espeak-ng/src/libespeak-ng/compiledata.c",
-        "espeak-ng/src/libespeak-ng/compiledict.c",
-        "espeak-ng/src/libespeak-ng/compilembrola.c",
-        "espeak-ng/src/libespeak-ng/dictionary.c",
-        "espeak-ng/src/libespeak-ng/encoding.c",
-        "espeak-ng/src/libespeak-ng/error.c",
-        "espeak-ng/src/libespeak-ng/espeak_api.c",
-        "espeak-ng/src/libespeak-ng/ieee80.c",
-        "espeak-ng/src/libespeak-ng/intonation.c",
-        "espeak-ng/src/libespeak-ng/mnemonics.c",
-        "espeak-ng/src/libespeak-ng/numbers.c",
-        "espeak-ng/src/libespeak-ng/readclause.c",
-        "espeak-ng/src/libespeak-ng/phoneme.c",
-        "espeak-ng/src/libespeak-ng/phonemelist.c",
-        "espeak-ng/src/libespeak-ng/setlengths.c",
-        "espeak-ng/src/libespeak-ng/soundicon.c",
-        "espeak-ng/src/libespeak-ng/spect.c",
-        "espeak-ng/src/libespeak-ng/speech.c",
-        "espeak-ng/src/libespeak-ng/ssml.c",
-        "espeak-ng/src/libespeak-ng/synthdata.c",
-        "espeak-ng/src/libespeak-ng/synthesize.c",
-        "espeak-ng/src/libespeak-ng/synth_mbrola.c",
-        "espeak-ng/src/libespeak-ng/translate.c",
-        "espeak-ng/src/libespeak-ng/tr_languages.c",
-        "espeak-ng/src/libespeak-ng/voices.c",
-        "espeak-ng/src/libespeak-ng/wavegen.c",
+
+        //"espeak-ng/src/ucd-tools/src/scripts.c",
+        //"espeak-ng/src/ucd-tools/src/tostring.c",
+        //"espeak-ng/src/libespeak-ng/compiledata.c",
+        //"espeak-ng/src/libespeak-ng/compilembrola.c",
+        //"espeak-ng/src/libespeak-ng/espeak_api.c",
+        //"espeak-ng/src/libespeak-ng/ieee80.c",
+        //"espeak-ng/src/libespeak-ng/soundicon.c",
+        //"espeak-ng/src/libespeak-ng/spect.c",
+        //"espeak-ng/src/libespeak-ng/ssml.c",
         "espeak-ng/src/ffi.c",
     ];
     let espeak_includes = vec![
@@ -65,6 +67,7 @@ fn main() {
     for src in espeak_srcs {
         base_config.file(src);
     }
+    base_config.define("XOUS", None);
 	base_config.compile("libespeak.a");
 }
 
