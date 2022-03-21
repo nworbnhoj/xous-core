ab23913031aa5eb56dae45eb24ec9bfffbf3148b
nworbnhoj <nworbnhoj@gmail.com>
2022-03-22 10:39:29 +1100

    add regular keep-alive-request Opcode

--- services/websocket/src/api.rs
+++ services/websocket/src/api.rs
@@ -1,9 +1,18 @@
+use std::{sync::Arc, time::Duration};
+
+
 #[allow(dead_code)]
 pub(crate) const SERVER_NAME_WEBSOCKET: &str     = "_Websocket Service_";
 
 
+pub(crate) const KEEPALIVE_TIMEOUT_SECONDS: Duration = Duration::from(55);
+pub(crate) const URL_LENGTH_LIMIT: usize = 200;
+
 /// These opcodes can be called by anyone at any time
 #[derive(num_derive::FromPrimitive, num_derive::ToPrimitive, Debug)]
 pub(crate) enum Opcode {
+    /// send a KeepAliveRequest
+    Tick,
+    /// Close Websocket and shutdown server
     Quit,
 }
--- services/websocket/src/main.rs
+++ services/websocket/src/main.rs
@@ -4,7 +4,15 @@
 mod api;
 use api::*;
 
+use std::{sync::Arc, time::Duration};
 use num_traits::FromPrimitive;
+use std::convert::TryInto;
+use std::thread;
+
+use embedded_websocket::*;
+use libsignal_services::*;
+
+
 
 
 #[xous::xous_main]
@@ -16,12 +24,36 @@ fn xmain() -> ! {
     let xns = xous_names::XousNames::new().unwrap();
     let ws_sid = xns.register_name(api::SERVER_NAME_WEBSOCKET, None).expect("can't register server");
     log::trace!("registered with NS -- {:?}", ws_sid);
+    
+    // build a thread that emits a regular WebSocketOp::Tick to send a KeepAliveRequest
+    thread::spawn({
+        let local_cid = xous::connect(ws_sid).unwrap();
+        move || {
+            let tt = ticktimer_server::Ticktimer::new().unwrap();
+            loop {
+                tt.sleep_ms(KEEPALIVE_TIMEOUT_SECONDS.as_millis).unwrap();
+                xous::send_message(local_cid,
+                    xous::Message::new_scalar(Opcode::Tick.to_usize().unwrap(),
+                    KEEPALIVE_TIMEOUT_SECONDS, 0, 0, 0)
+                ).expect("couldn't send Websocket tick");
+            }
+        }
+    });
 
 
     log::trace!("ready to accept requests");
     loop {
         let mut msg = xous::receive_message(ws_sid).unwrap();
         match FromPrimitive::from_usize(msg.body.id()) {
+            Some(Opcode::Tick) => {
+                if let Err(e) =
+                    incoming_sink.send(WebSocketStreamItem::KeepAliveRequest)
+                {
+                    log::info!("Websocket sink has closed: {:?}.", e);
+                    break;
+                }
+                log::info!("Websocket keep-alive request sent");
+            },
             Some(Opcode::Quit) => {
                 log::warn!("got quit!");
                 break
