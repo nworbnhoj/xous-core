2134d19c0129a40cc5e2eddb95398d0e9351217e
nworbnhoj <nworbnhoj@gmail.com>
2022-03-30 17:38:30 +1100

    add Open Opcode

--- services/websocket/src/api.rs
+++ services/websocket/src/api.rs
@@ -1,18 +1,44 @@
-use std::{sync::Arc, time::Duration};
-
-
 #[allow(dead_code)]
-pub(crate) const SERVER_NAME_WEBSOCKET: &str     = "_Websocket Service_";
+pub(crate) const SERVER_NAME_WEBSOCKET: &str = "_Websocket Service_";
 
+use std::time::Duration;
 
-pub(crate) const KEEPALIVE_TIMEOUT_SECONDS: Duration = Duration::from(55);
+pub(crate) const KEEPALIVE_TIMEOUT_SECONDS: Duration = Duration::from_secs(55);
 pub(crate) const URL_LENGTH_LIMIT: usize = 200;
+
+pub(crate) const BASEURL_LEN: usize = 128;
+pub(crate) const PATH_LEN: usize = 128;
+pub(crate) const LOGIN_LEN: usize = 128;
+pub(crate) const PASSWORD_LEN: usize = 128;
+
+/*
+ WEBSOCKET_BUFFER_LEN can be as small as 14bytes, but presumably comes with a performance degradation.
+ ( see https://crates.io/crates/embedded-websocket )
+ Also: there may be advantage in independently specifying the read, frame, and write buffer sizes.
+ TODO review/test/optimise WEBSOCKET_BUFFER_LEN
+*/
+pub(crate) const WEBSOCKET_BUFFER_LEN: usize = 4000;
 
 /// These opcodes can be called by anyone at any time
 #[derive(num_derive::FromPrimitive, num_derive::ToPrimitive, Debug)]
 pub(crate) enum Opcode {
+    /// close and existing websocket
+    Close = 1,
+    /// open a new websocket
+    Open,
     /// send a KeepAliveRequest
     Tick,
     /// Close Websocket and shutdown server
     Quit,
+}
+
+// a structure for defining the setup of a Websocket.
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize)]
+pub struct WebsocketConfig {
+    //    pub tls_config: rustls::ClientConfig,   // requires manual implementation of Archive....
+    pub base_url: xous_ipc::String<BASEURL_LEN>,
+    pub path: xous_ipc::String<PATH_LEN>,
+    pub use_credentials: bool,
+    pub login: xous_ipc::String<LOGIN_LEN>,
+    pub password: xous_ipc::String<PASSWORD_LEN>,
 }
--- services/websocket/src/main.rs
+++ services/websocket/src/main.rs
@@ -3,17 +3,33 @@
 
 mod api;
 use api::*;
+use embedded_websocket::{
+    framer::Framer, WebSocketClient, WebSocketCloseStatusCode, WebSocketOptions,
+    WebSocketSendMessageType,
+};
+use num_traits::{FromPrimitive, ToPrimitive};
+use rustls_connector::*;
+use std::{
+    collections::HashMap,
+    convert::TryInto,
+    io::{Error, Read, Write},
+    net::TcpStream,
+    thread,
+};
+use url::Url;
+use xous_ipc::Buffer;
 
-use std::{sync::Arc, time::Duration};
-use num_traits::FromPrimitive;
-use std::convert::TryInto;
-use std::thread;
+struct WssStream<T: Read + Write>(T);
 
-use embedded_websocket::*;
-use libsignal_services::*;
+impl<T: Read + Write> embedded_websocket::framer::Stream<Error> for WssStream<T> {
+    fn read(&mut self, buf: &mut [u8]) -> std::result::Result<usize, Error> {
+        self.0.read(buf)
+    }
 
-
-
+    fn write_all(&mut self, buf: &[u8]) -> std::result::Result<(), Error> {
+        self.0.write_all(buf)
+    }
+}
 
 #[xous::xous_main]
 fn xmain() -> ! {
@@ -22,41 +38,182 @@ fn xmain() -> ! {
     log::info!("my PID is {}", xous::process::id());
 
     let xns = xous_names::XousNames::new().unwrap();
-    let ws_sid = xns.register_name(api::SERVER_NAME_WEBSOCKET, None).expect("can't register server");
+    let ws_sid = xns
+        .register_name(api::SERVER_NAME_WEBSOCKET, None)
+        .expect("can't register server");
     log::trace!("registered with NS -- {:?}", ws_sid);
-    
+
     // build a thread that emits a regular WebSocketOp::Tick to send a KeepAliveRequest
     thread::spawn({
         let local_cid = xous::connect(ws_sid).unwrap();
         move || {
             let tt = ticktimer_server::Ticktimer::new().unwrap();
             loop {
-                tt.sleep_ms(KEEPALIVE_TIMEOUT_SECONDS.as_millis).unwrap();
-                xous::send_message(local_cid,
-                    xous::Message::new_scalar(Opcode::Tick.to_usize().unwrap(),
-                    KEEPALIVE_TIMEOUT_SECONDS, 0, 0, 0)
-                ).expect("couldn't send Websocket tick");
+                tt.sleep_ms(KEEPALIVE_TIMEOUT_SECONDS.as_millis().try_into().unwrap())
+                    .unwrap();
+                xous::send_message(
+                    local_cid,
+                    xous::Message::new_scalar(
+                        Opcode::Tick.to_usize().unwrap(),
+                        KEEPALIVE_TIMEOUT_SECONDS.as_secs().try_into().unwrap(),
+                        0,
+                        0,
+                        0,
+                    ),
+                )
+                .expect("couldn't send Websocket tick");
             }
         }
     });
 
+    /*
+    These buffers can be allocated here before the main loop, or alternatvely alocated and
+    de-allocated within the loop as required ( presumably, trading memory with performance )
+    TODO review/test/optimise buffer allocation
+    */
+    let mut read_buf = [0; WEBSOCKET_BUFFER_LEN];
+    let mut read_cursor = 0;
+    let mut write_buf = [0; WEBSOCKET_BUFFER_LEN];
+    let mut _frame_buf = [0; WEBSOCKET_BUFFER_LEN];
+    let mut _read_buf = [0; WEBSOCKET_BUFFER_LEN];
+    let mut websocket_client = WebSocketClient::new_client(rand::thread_rng());
+    let mut framer = Framer::new(
+        &mut read_buf,
+        &mut read_cursor,
+        &mut write_buf,
+        &mut websocket_client,
+    );
+
+    /*
+    This HashMap holds open streams by pid - and as such - limits each pid to 1 websocket.
+    TODO review the limitation of 1 websocket per pid.
+    */
+    let mut streams = HashMap::new();
 
     log::trace!("ready to accept requests");
     loop {
         let mut msg = xous::receive_message(ws_sid).unwrap();
         match FromPrimitive::from_usize(msg.body.id()) {
-            Some(Opcode::Tick) => {
-                if let Err(e) =
-                    incoming_sink.send(WebSocketStreamItem::KeepAliveRequest)
-                {
-                    log::info!("Websocket sink has closed: {:?}.", e);
+            Some(Opcode::Close) => {
+                let pid = msg.sender.pid().unwrap();
+                let mut stream = match streams.remove(&pid) {
+                    Some(stream) => stream,
+                    None => {
+                        log::info!("Websocket stream not found in list");
+                        break;
+                    }
+                };
+
+                match framer.close(&mut stream, WebSocketCloseStatusCode::NormalClosure, None) {
+                    Ok(()) => log::info!("Sent close handshake"),
+                    Err(e) => log::info!("Failed to send close handshake {:?}", e),
+                };
+            }
+            Some(Opcode::Open) => {
+                let pid = msg.sender.pid().unwrap();
+                if streams.contains_key(&pid) {
+                    log::info!("WebSocket already open for pid: {:?}", pid);
                     break;
                 }
+                let buf = unsafe {
+                    Buffer::from_memory_message_mut(msg.body.memory_message_mut().unwrap())
+                };
+                let ws_config = buf.to_original::<WebsocketConfig, _>().unwrap();
+
+                let base_url = ws_config
+                    .base_url
+                    .as_str()
+                    .expect("base_url utf-8 decode error");
+                let mut url = Url::parse(base_url).expect("valid base_url");
+                let path = ws_config.path.as_str().expect("path utf-8 decode error");
+                url = url.join(path).expect("valid path");
+                url.set_scheme("wss").expect("valid https base url");
+
+                log::trace!("Will start websocket at {:?}", url.as_str());
+
+                if ws_config.use_credentials {
+                    let login = ws_config.login.as_str().expect("login utf-8 decode error");
+                    let password = ws_config
+                        .password
+                        .as_str()
+                        .expect("password utf-8 decode error");
+                    url.query_pairs_mut()
+                        .append_pair("login", &login)
+                        .append_pair("password", &password);
+                }
+
+                // Create a TCP Stream between this client and the remote Server
+                let tcp_stream = match TcpStream::connect(url.as_str()) {
+                    Ok(tcp_stream) => tcp_stream,
+                    Err(e) => {
+                        log::info!("Failed to open TCP Stream {:?}", e);
+                        break;
+                    }
+                };
+                log::info!("TCP connected to {:?}", base_url);
+
+                // Create a TLS connection to the remote Server on the TCP Stream
+                // TODO pass ssl_config in during Opcode::Open ie ::from(ws_config.ssl_config);
+                let tls_connector = RustlsConnector::from(local_ssl_config());
+                let tls_stream = match tls_connector.connect(base_url, tcp_stream) {
+                    Ok(stream) => {
+                        log::info!("TLS connected to {:?}", base_url);
+                        stream
+                    }
+                    Err(e) => {
+                        log::info!("Failed to complete TLS handshake {:?}", e);
+                        break;
+                    }
+                };
+
+                // Initiate a websocket opening handshake over the TLS Stream
+                let websocket_options = WebSocketOptions {
+                    path: &path,
+                    host: &base_url,
+                    origin: "",
+                    sub_protocols: None,
+                    additional_headers: None,
+                };
+                let mut wss_stream = WssStream(tls_stream);
+                let sub_protocol = match framer.connect(&mut wss_stream, &websocket_options) {
+                    Ok(sub_protocol) => sub_protocol,
+                    Err(e) => {
+                        log::info!("Unable to connect WebSocket {:?}", e);
+                        break;
+                    }
+                };
+                // TODO maybe subprotol should be saved in options??
+                log::info!("WebSocket connected with: {:?}", sub_protocol);
+
+                // Store the open websocket indexed by the calling pid
+                streams.insert(pid, wss_stream);
+            }
+            Some(Opcode::Tick) => {
+                let pid = msg.sender.pid().unwrap();
+                let stream = match streams.get_mut(&pid) {
+                    Some(stream) => stream,
+                    None => {
+                        log::info!("Websocket stream not in list");
+                        break;
+                    }
+                };
+                let frame_buf = "keep alive please :-)".as_bytes();
+                match framer.write(
+                    &mut *stream,
+                    WebSocketSendMessageType::Text,
+                    true,
+                    &frame_buf,
+                ) {
+                    Ok(()) => log::info!("Websocket keep-alive request sent"),
+                    Err(e) => log::info!("failed to send Websocket keep-alive request {:?}", e),
+                };
+
                 log::info!("Websocket keep-alive request sent");
-            },
+            }
+
             Some(Opcode::Quit) => {
                 log::warn!("got quit!");
-                break
+                break;
             }
             None => {
                 log::error!("couldn't convert opcode: {:?}", msg);
@@ -69,4 +226,27 @@ fn xmain() -> ! {
     xous::destroy_server(ws_sid).unwrap();
     log::trace!("quitting");
     xous::terminate_process(0)
+}
+
+// TODO pass ssl_config in during Opcode::Open ie ::from(ws_config.ssl_config);
+// this is temporary Signal specific code below
+
+use libsignal_service::configuration::{ServiceConfiguration, SignalServers};
+
+fn local_ssl_config() -> rustls::ClientConfig {
+    let cfg: ServiceConfiguration = SignalServers::Staging.into();
+    //    let user_agent = "xous".to_string();
+    let mut cert_bytes = std::io::Cursor::new(&cfg.certificate_authority);
+    let roots = rustls_pemfile::certs(&mut cert_bytes).expect("parseable PEM files");
+    let roots = roots.iter().map(|v| rustls::Certificate(v.clone()));
+
+    let mut root_certs = rustls::RootCertStore::empty();
+    for root in roots {
+        root_certs.add(&root).unwrap();
+    }
+
+    rustls::ClientConfig::builder()
+        .with_safe_defaults()
+        .with_root_certificates(root_certs)
+        .with_no_client_auth()
 }
